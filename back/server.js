0e771c887ace969a749cf6797fa9587105a7f5d4
Your Name <you@example.com>
2018-07-14 20:31:30 +0200

    add changes

--- back/server.js
+++ back/server.js
@@ -1,8 +1,10 @@
+const express = require('express')
+const app = express()
 
-const express = require('express');
-const app = express();
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
 
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', function(req, res) {
-  res.send('hello world');
-});
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
