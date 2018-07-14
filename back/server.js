1aa1bdbd908e9bcd6423f0015f282abcc792d87d
Your Name <you@example.com>
2018-07-14 21:12:57 +0200

    add changes

--- back/server.js
+++ back/server.js
@@ -11,5 +11,5 @@ app.get('/test', function (req, res) {
   })
   
 app.listen(8080, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 8080!')
 })
