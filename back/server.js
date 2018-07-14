4b6cf0795096a234c8f89655917e7b06db75c2e2
Your Name <you@example.com>
2018-07-14 20:28:36 +0200

    add changes

---
+++ back/server.js
@@ -0,0 +1,8 @@
+
+const express = require('express');
+const app = express();
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function(req, res) {
+  res.send('hello world');
+});
