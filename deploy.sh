a06d9e2b943ce020b66bb0382b7f38f772a1bb94
Your Name <you@example.com>
2018-07-15 11:46:53 +0200

    add changes

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 echo -e "\033[32m Begin Deploy.."
-echo -ne '#####                     (0%)\r'
+echo -ne '\033[32m  ###############                     (0%)\r'
 git pull origin master
-echo -ne '#####                     (10%)\r'
+echo -ne '\033[32m ###############                     (10%)\r'
 cd back
 npm install --production
-echo -ne '#####                     (30%)\r'
+echo -ne '\033[32m ###############                     (30%)\r'
 
 cd ../front
 npm install  --production
-echo -ne '#####                     (60%)\r'
+echo -ne '\033[32m ###############                     (60%)\r'
 
 npm run build
-echo -ne '#####                     (80%)\r'
+echo -ne '\033[32m ###############                     (80%)\r'
 
 pm2 restart bo server
-echo -ne '#####                     (1000%)\r'
+echo -ne '\033[32m ###############                     (1000%)\r'
 
 echo -e "\033[32m End Deploy."
