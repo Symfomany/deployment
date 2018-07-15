f2b6ef50ea71d479d6b064a4f4b42b15ade281ce
Your Name <you@example.com>
2018-07-15 08:30:50 +0200

    add changes

--- back/bo.js
+++ back/bo.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const compression = require('compression');
 
 app.use(cors())
+app.use(compression()); //Compress all routes
 
 
 app.get('/', function (req, res) {
