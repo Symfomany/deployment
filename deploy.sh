aa149fbf17a66e0a5efa6de75ab0e9043a15d016
Your Name <you@example.com>
2018-07-16 06:56:42 +0200

    last changes

--- deploy.sh
+++ deploy.sh
@@ -18,29 +18,29 @@ echo -ne '\033[32m ###############                     (5%)\r'
 
 sudo su <<EOF
     ssh root@159.89.15.125
+
+    cd /home/deployment/
+
+    git pull origin master
+    echo -ne '\033[32m ###############                     (10%)\r'
+    cd back
+    npm install --production
+    echo -ne '\033[32m ###############                     (30%)\r'
+
+
+    cd ../front
+    npm cache clean --force
+    npm install  --production
+    echo -ne '\033[32m ###############                     (60%)\r'
+
+    npm run build
+    echo -ne '\033[32m ###############                     (80%)\r'
+
+    # SSH Key
+    # cp -R .well-known/ build/
+
+    pm2 restart bo server
+    echo -ne '\033[32m ###############                     (1000%)\r'
+
+    echo -e "\033[32m End Deploy."
 EOF
-
-cd /home/deployment/
-
-git pull origin master
-echo -ne '\033[32m ###############                     (10%)\r'
-cd back
-npm install --production
-echo -ne '\033[32m ###############                     (30%)\r'
-
-
-cd ../front
-npm cache clean --force
-npm install  --production
-echo -ne '\033[32m ###############                     (60%)\r'
-
-npm run build
-echo -ne '\033[32m ###############                     (80%)\r'
-
-# SSH Key
-# cp -R .well-known/ build/
-
-pm2 restart bo server
-echo -ne '\033[32m ###############                     (1000%)\r'
-
-echo -e "\033[32m End Deploy."
